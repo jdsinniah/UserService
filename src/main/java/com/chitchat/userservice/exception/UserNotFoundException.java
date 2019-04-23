498ac2575ac3a55c297106e62dd1dfd6202c4933
jdsinniah <dario.sinniah@hotmail.it>
2019-04-23 10:31:27 +0200

    Changed UserNotFoundException to extend RuntimeException.
    Signed-off-by: jdsinniah <dario.sinniah@hotmail.it>

--- src/main/java/com/chitchat/userservice/exception/UserNotFoundException.java
+++ src/main/java/com/chitchat/userservice/exception/UserNotFoundException.java
@@ -1,6 +1,6 @@
 package com.chitchat.userservice.exception;
 
-public class UserNotFoundException extends Exception {
+public class UserNotFoundException extends RuntimeException {
     public UserNotFoundException(String message) {
         super(message);
     }
